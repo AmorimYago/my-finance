595e937a61afe35da599c9a2378910d4aeecd491
Yago Amorim <Swashbuckler3113@outlook.com>
2025-08-07 19:28:20 -0300

    chore: financial base project with Next.js, Auth, Prisma, and Docker

---
+++ src/lib/auth/authOptions.ts
@@ -0,0 +1,21 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import GoogleProvider from "next-auth/providers/google"
+import type { NextAuthOptions } from "next-auth"
+import { prisma } from "@/lib/prismaClient"
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "database",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  // pages: {
+    //signIn: "/login", // vamos criar essa tela depois
+  //},
+}
